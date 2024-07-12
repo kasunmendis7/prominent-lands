06cca6968a0e4826dfdf2c6a9859b85af7bd10c4
Kasun Mendis <tkomendis@gmail.com>
2024-07-12 19:14:26 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.js";
+import SignIn from "./pages/SignIn.js";
+import SignUp from "./pages/SignUp.js";
+import About from "./pages/About.js";
+import ContactUs from "./pages/ContactUs.js";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
